dc351239da33cbdc4c97e19fbf475b63dc1e4685
fayssalbidari <146531738+fayssalbidari@users.noreply.github.com>
2024-05-15 16:37:54 +0200

    Responsive About
    Mise en place du responsive pour la page about

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,8 @@ function menuOpen(){
 
 }
 
+/* if page home */
+
 let timeAbout = gsap.timeline({
     scrollTrigger: {
         trigger: ".about",
@@ -32,7 +34,7 @@ timeAbout.to('.about-container__img', {
     rotate: ' 2deg'
 });
 
-
+/*
 let timeWork = gsap.timeline({
     scrollTrigger: {
         trigger: ".work",
@@ -98,75 +100,8 @@ timeWork.to(".projet-3", {
 });
 
 timeWork.to('.projet-3', {});
-
-/*
-let timeProjet1 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".projet-1",
-        pin: "true",
-        end: "+=1000",
-        pinSpacing: 'true',
-        scrub: 1
-    }
-});
-timeProjet1.to('.images-1', {
-    y: '0px',
-});
-
-let timeProjet2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".projet-2",
-        pin: "true",
-        end: "+=1000",
-        pinSpacing: 'true',
-        scrub: 1
-    }
-});
-timeProjet2.to('.images-2', {
-    y: '0px',
-});
-
-let timeProjet1 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".projet-1",
-        pin: "true",
-        end: "+=2000",
-        pinSpacing: 'true',
-        scrub: 1
-    }
-});
-timeProjet1.to('.images-1', {
-    y: '0px',
-});
-
-let timeProjet2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".projet-2",
-        pin: "true",
-        start:'bottom bottom',
-        end: "+=500",
-        pinSpacing: 'true',
-        scrub: 1
-    }
-});
-timeProjet2.to('.images-2', {
-    y: '0px',
-});
-
-let timeProjet3 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".projet__el",
-        pin: "true",
-        start: 'bottom top',
-        end: "+=1000",
-        pinSpacing: 'true',
-        scrub: 1
-    }
-});
-timeProjet3.to('.images-3', {
-    y: '0px',
-});
 */
+
 let timeSkills = gsap.timeline({
     scrollTrigger: {
         trigger: ".skills__item",
@@ -180,56 +115,14 @@ timeSkills.to('.skills__item', {
     x: '0px',
 });
 
-/*
-let timeln = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".cards",
-        pin: true,
-        pinSpacing: true,
-        start: "left-=120px left",
-        end: "+=2000",
-        scrub: 1
-    }
-});
 
-timeln.addLabel('card1');
-timeln.to('.card-1', {
-    xPercent: 0,
-    opacity: 1
-});
+/* if page work */
 
-timeln.from('.card-2', {
-    xPercent: 75,
-    opacity: 0
-});
-timeln.addLabel("card2");
 
-timeln.to(".card-1", {
-    scale: 0.95,
-    xPercent: -0.5,
-    opacity: 0.5
-}, "-=0.3");
+const btn = document.querySelector(".switch");
 
-timeln.to('.card-2', {
-    xPercent: 0,
-    opacity: 1
-});
+btn.addEventListener("click", switchToggle);
 
-timeln.from('.card-3', {
-    xPercent: 75,
-    opacity: 0
-});
-timeln.addLabel('card3');
-
-timeln.to(".card-2", {
-    scale: 0.98,
-    xPercent: -0.4,
-    opacity: 0.6
-}, "-=0.3");
-
-timeln.to(".card-3", {
-    xPercent: 0,
-    opacity: 1
-});
-
-*/
+function switchToggle() {
+    btn.classList.toggle("switch--active");
+}
